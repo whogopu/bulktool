c0957068979b606d3c75b91406ddaacb717e5d8d
Gopal sharma <whogopu@gmail.com>
2022-03-27 11:42:25 +0530

    10 tests

--- scripts/liveDesktop.sh
+++ scripts/liveDesktop.sh
@@ -2,4 +2,4 @@
 # liveDesktop
 source ~/.nvm/nvm.sh
 cd /home/gopal/projects/my/bulktool
-nvm install stable && nvm use stable && rm urls.csv && cp urls/url_live_bigSet.csv urls.csv && export DEVICE=desktop && export TEST=20 && export CHUNK=20 && npm start
+nvm install stable && nvm use stable && rm urls.csv && cp urls/url_live_bigSet.csv urls.csv && export DEVICE=desktop && export TEST=10 && export CHUNK=20 && npm start
--- scripts/liveMobile.sh
+++ scripts/liveMobile.sh
@@ -2,4 +2,4 @@
 #liveMobile
 source ~/.nvm/nvm.sh
 cd /home/gopal/projects/my/bulktool
-nvm install stable && nvm use stable && rm urls.csv && cp urls/url_live_bigSet.csv urls.csv && export DEVICE=mobile && export TEST=20 && export CHUNK=20 && npm start
+nvm install stable && nvm use stable && rm urls.csv && cp urls/url_live_bigSet.csv urls.csv && export DEVICE=mobile && export TEST=10 && export CHUNK=20 && npm start
