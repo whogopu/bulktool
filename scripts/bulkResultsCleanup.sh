7b9cda8b2fad159d901f80f1ae6b28df067f4e76
Gopal sharma <gopal.sharma@99acres.com>
2022-05-03 14:08:37 +0530

    updated scripts

--- scripts/bulkResultsCleanup.sh
+++ scripts/bulkResultsCleanup.sh
@@ -3,11 +3,12 @@
 # trap 'exec 2>&4 1>&3' 0 1 2 3
 # exec 1>log.out 2>&1
 
-cd /home/gopal/projects/my/bulktool/results
+base="/home/gopal/projects/my/bulktool/results"
+cd "$base"
 NOW=$(date +"%Y-%m-%d")
 echo -e "Started at $NOW"
 for d in */ ; do
-    echo -e "\nprocessing $d"
+    echo -e "\nprocessing $d folder"
 
     IFS='-' read -ra ADDR <<< "$d"
     arraylength=${#ADDR[@]}
@@ -22,14 +23,32 @@ for d in */ ; do
    
     if [ $DATE_DIFF -gt 30 ]
     then
-      echo "Delete it - $d"
+      echo "Deleting the folder"
       rm -rf "$d"
     elif [ $DATE_DIFF -gt 8 ]
     then
-      echo "Delete the json files"
+      echo "Deleting the json files of folder"
       rm -rf "$d/json"
     else
-      echo "Retain it"
+      echo "Can Retain the folder - Now checking median file"
+      # echo "$base/$d"
+      cd "$base/$d"
+      count=$(ls | wc -l)
+      # for f in * ; do
+      #   echo -e "\n file $f"
+      # done
+      # echo $count
+      if [ $count -gt 1 ]
+      then
+        echo "This folder has median files - Retaining this directory"
+      elif [ $DATE_DIFF -gt 0 ]
+      then
+        echo "This folder is older than 1 day and not having median files - remove this directory"
+        cd "$base"
+        rm -rf "$d"
+      else
+        echo "It is todays' folder - ignoring it"
+      fi
     fi
 done
 echo -e "\nDone !! \n"
